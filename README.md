88e4adc532e36235d38c16874c4423c76ae1624e
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-07 22:03:41 +0100

    Big Update! (Read Description)
    Added jQuery to ease coding,
    Which in turn made me want to make generators able to be dynamically
    generated, so I did. It was a rather hard task, as you can see.
    I’ve slightly updated the styles, and added a few more generators.
    After this update I’m starting to work on exponential growth for the
    costs.
    (And updated the README)

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Another Idle Game
-A small idle game made as an exercise in JS.
-# Features so far
-nothing.
+A game built with jQuery(And, therefore, JS) which is still a work in progress.
+# Current Features
+Clicking to make money, woo!
+Buying generators, woo!
+# Planned Features
+Upgrading those generators, woo!
+Achievements, woo!
+Settings, woo!
+And much more, woo!
+# Location
+https://mysteriousmagenta.github.io/another-idle-game/
--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,30 @@
-var moneyLimit = 100000000  //Small limit for now
+if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+	alert("The File API is not supported by your browser, you will not be able to play without that.");
+	var body = $("body")
+	for (var i = 0; i < body.children.length; i++) {
+		var child = body.children[i];
+		child.fadeOut("slow");
+	};
+}
+else {
 var player = {
-	"money": 0,
-
+	"money": 0
 }
 var generators = []
+var generatorTemplates = [
+	{"name": "My First Generator", "mps": 1, "cost": 10},
+	{"name": "Better Faster Harder Generator", "mps": 10, "cost": 75},
+	{"name": "STL Generator", "mps": 100, "cost": 500},
+	{"name": "Slightly Less STL Generator", "mps": 250, "cost": 750},
+	{"name": "Steampunk Generator", "mps": 1000, "cost": 1250}
+]
 
-function gameWon() {
-	console.log("Game won.");
-}
-
-
+// Helper Functions
 function addMoney(amount) {
 	if (amount === undefined || amount === null) {
 		amount = 1
 	}
 	player.money += amount;
-	if (player.money > moneyLimit) {
-		gameWon();
-	}
 }
 
 function makeGenerator(name, mps, cost) {
@@ -33,6 +40,16 @@ function makeGenerator(name, mps, cost) {
 	}
 }
 
+function countGenerators(name) {
+	var count = 0;
+	for (var i = 0; i < generators.length; i++) {
+		if (generators[i].name == name) {
+			count++
+		}
+	};
+	return count;
+}
+
 function activateGen() {
 	var generator;
 	for (var i=0; i < generators.length; i++) {
@@ -42,19 +59,56 @@ function activateGen() {
 }
 
 function updateDisplay() {
-	var display = document.getElementById("display");
-	display.innerText = "€" + player.money;
-	var persecond = document.getElementById("mps");
+	$("#display").html("€" + player.money);
 	var total = 0;
 	for (var i = 0; i < generators.length; i++) {
 		total += generators[i].mps;
 	};
-	persecond.innerText = "€" + total + "/S";
+	$("#mps").html("€" + total + "/S");
 }
 
+function makeGeneratorList() {
+	var generatorList = $("#generators");
+	if (generatorList.children.length) {
+		for (var i = 0; i < generatorList.children.length; i++) {
+			generatorList.children[i].remove();
+		};
+	}
+	for (var i = 0; i < generatorTemplates.length; i++) {
+		var generator = generatorTemplates[i];
+		var generatorObj = $('<li class="generator" name="'+ generator.name + '" cost="' + generator.cost + '" mps="'+ generator.mps + '">' + generator.name + "</li>")
+
+		generatorObj.mouseenter(function() {
+			var jThis = $(this);
+			var newName = jThis.html();
+			newName += " [€" + jThis.attr("cost") + "]";
+			newName += " [€" + jThis.attr("mps") + "/S]";
+			newName += " [" + countGenerators(jThis.attr("name")) + "]"
+			jThis.html(newName);
+		});
+		generatorObj.mouseleave(function() {
+			var jThis = $(this);
+			jThis.html(jThis.attr("name"));
+		});
+		generatorObj.click(function() {
+				var jThis = $(this);
+				makeGenerator(jThis.attr("name"), +jThis.attr("mps"), +jThis.attr("cost"));
+				jThis.mouseleave();
+				jThis.mouseenter();
+		});
+		generatorList.append(generatorObj);
+		if ((i+1) % 3 === 0) {
+			generatorList.append($("<br/>"))
+		}
+	};
+}
+// Starts the game
 function initGame() {
+	makeGeneratorList();
 	setInterval(activateGen, 1000);
 	setInterval(updateDisplay, 100);
 }
 
+
 window.onload = initGame;
+}
