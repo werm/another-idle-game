f07fb541fc7ee9acce0ba8d519f89ffe05884c7f
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-10 00:52:06 +0100

    WARNING: GAME UNSTABLE

--- scripts/game.js
+++ scripts/game.js
@@ -148,6 +148,7 @@ function makeTooltips() {
 function getMoneyCookie() {
 	var money = document.cookie.substr(document.cookie.indexOf("money=")+"money=".length, document.cookie.length);
 	var generatorList = $("#generators td");
+	console.log(generatorList);
 	if (money) {
 		for (var i = 0; i < money.length; i++) {
 			if (money[i]===";") {
