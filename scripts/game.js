1edf581caae053ba29fb372ccd60a02082e914fe
MysteriousMagenta <therealrebuilder@gmail.com>
2015-03-10 00:56:35 +0100

    Should work now.

--- scripts/game.js
+++ scripts/game.js
@@ -148,10 +148,6 @@ function makeTooltips() {
 function getMoneyCookie() {
 	var money = document.cookie.substr(document.cookie.indexOf("money=")+"money=".length, document.cookie.length);
 	var generatorList = $("#generators tr td");
-	for (var i = 0; i < generatorList.length; i++) {
-		console.log(generatorList[i]);
-	};
-	return;
 	if (money) {
 		for (var i = 0; i < money.length; i++) {
 			if (money[i]===";") {
